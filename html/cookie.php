acc035d96bad0b0dcc168250192d3ea9bd8919a1
parsa dolat <parsafeynman@gmail.com>
2025-12-07 18:39:41 +0000

    added cookie

--- html/cookie.php
+++ html/cookie.php
@@ -1,5 +1,5 @@
 <?php
 // Set a test cookie with explicit options
-setcookie('test', 'test', time() + 3600, '/', 'hountybunter.click', false, true);
+setcookie('test', 'test', time() + 3600, '/', '.hountybunter.click', false, true);
 
 echo 'Cookie set';
